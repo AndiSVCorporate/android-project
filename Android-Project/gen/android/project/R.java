4cb58a9cb250cc216ba76b539c0a4b0c451f82e3
sameid <sameid@gmail.com>
2011-05-05 20:29:37 +0000

    added pictures

--- Android-Project/gen/android/project/R.java
+++ Android-Project/gen/android/project/R.java
@@ -11,8 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int fireman=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int epicfailgamingstudios=0x7f020000;
+        public static final int fireman=0x7f020001;
+        public static final int gamelogo=0x7f020002;
+        public static final int icon=0x7f020003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
